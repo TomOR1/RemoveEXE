eb019483263c09515096ea00dde32cc31b12b792
TomOR <thomasor@live.com>
2018-07-11 12:37:55 +0100

    Add files via upload

---
+++ PreventEXE/PreventEXE/Main.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace PreventEXE
+{
+    public class PreventEXE
+    {
+        void DissableExtentions(Peramiters perams)
+        {
+            // Error prevention
+
+            if (perams.DislikedExtentions == null || perams.DislikedExtentions.Length == 0)
+            {
+                perams.DislikedExtentions = new string[] { ".exe" };
+            }
+
+            if (perams.Suffix == null || perams.Suffix == "")
+            {
+                perams.Suffix = ".dissabled";
+            }
+
+            TemporaryHoldings[] temporaryHoldings = new TemporaryHoldings[perams.DislikedExtentions.Length];
+
+            for (int i = 0; i < perams.DislikedExtentions.Length; i++)
+            {
+                temporaryHoldings[i].List = Directory.GetFiles(perams.Path, "*" + perams.DislikedExtentions[i], SearchOption.AllDirectories);
+            }
+
+            for (int i = 0; i < temporaryHoldings.Length; i++)
+            {
+                for (int i1 = 0; i1 < temporaryHoldings[i].List.Length; i1++)
+                {
+                    try
+                    {
+                        File.Move(temporaryHoldings[i].List[i1], temporaryHoldings[i].List[i1] + perams.Suffix);
+                    }
+                    catch
+                    {
+                        System.Console.WriteLine("Error: Could not rename " + temporaryHoldings[i].List[i1] + ". This was most likely a privalage issue.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PreventEXE/PreventEXE/Peramiters.cs
@@ -0,0 +1,23 @@
+﻿namespace PreventEXE
+{
+    class Peramiters
+    {
+        /// <summary>
+        /// The origin
+        /// </summary>
+        public string Path { get; set; }
+        /// <summary>
+        /// (should start with a '.') What extentions to augment
+        /// </summary>
+        public string[] DislikedExtentions { get; set; }
+        /// <summary>
+        /// What to add to the end of the dissliked extentions
+        /// </summary>
+        public string Suffix { get; set; }
+    }
+
+    class TemporaryHoldings
+    {
+        public string[] List { get; set; }
+    }
+}
